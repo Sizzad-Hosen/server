193104d1a051c333f50e76827836c0dd9e0cc33e
Sizzad Hosen <sizzadhosenofficial@gmail.com>
2025-07-12 03:41:29 +0600

    alhamdullia all api test response successfully

--- src/modules/SubCategory/subcategory.route.ts
+++ src/modules/SubCategory/subcategory.route.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 
 
 router.post(
-  '/',
+  '/create-subCategory',
   validateRequest(SubCategoryValidationSchemas.createSubCategorySchema),
   SubCategoryControlles.createSubCategoryController
 );
