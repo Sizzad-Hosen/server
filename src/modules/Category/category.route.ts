0e928a13557cb56abad5d74ca8fde0e47ba8f4d4
Sizzad Hosen <sizzadhosenofficial@gmail.com>
2025-08-04 15:08:56 +0600

    auth guird set which admin and user

--- src/modules/Category/category.route.ts
+++ src/modules/Category/category.route.ts
@@ -2,21 +2,23 @@ import express from 'express';
 import { CategoryControllers, createCategoryController } from './category.controller';
 import validateRequest from '../../app/middlewares/validateRequest';
 import { CategoryValidationSchemas } from './category.validation';
+import auth from '../../app/middlewares/auth';
 
 const router = express.Router();
 
 
-router.post('/create-category', validateRequest(CategoryValidationSchemas.createCategorySchema),CategoryControllers.createCategoryController);
+router.post('/create-category',  auth("admin"), validateRequest(CategoryValidationSchemas.createCategorySchema),CategoryControllers.createCategoryController);
 
-router.get('/', CategoryControllers.getAllCategoriesController);
+router.get('/',  auth("admin","customer"), CategoryControllers.getAllCategoriesController);
 
-router.get('/categoryByServiceId/:serviceId', CategoryControllers.getCategoriesByServiceIdController);
-
-
-router.get('/:id', CategoryControllers.getSingleCategoryController);
-router.put('/:id', validateRequest(CategoryValidationSchemas.updateCategorySchema), CategoryControllers.updateCategoryController);
+router.get('/categoryByServiceId/:serviceId',auth("admin","customer"), CategoryControllers.getCategoriesByServiceIdController);
 
 
-router.delete('/:id', CategoryControllers.deleteCategoryController);
+router.get('/:id',auth("admin","customer"), CategoryControllers.getSingleCategoryController);
+
+router.put('/:id',  auth("admin"), validateRequest(CategoryValidationSchemas.updateCategorySchema), CategoryControllers.updateCategoryController);
+
+
+router.delete('/:id',  auth("admin"), CategoryControllers.deleteCategoryController);
 
 export const CategoryRoutes = router;
